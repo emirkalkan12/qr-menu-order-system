73a6a16c6ad30220ee3e8a3cef9e812a6d89742f
Emirhan KALKAN <emikalkann@gmail.com>
2025-05-15 15:12:53 +0300

    Add service index.js for centralized export of service modules

---
+++ src/services/index.js
@@ -0,0 +1,6 @@
+export { menuService } from './menuService';
+export { orderService } from './orderService';
+export { qrService } from './qrService';
+
+// Export mock data for direct access if needed
+export { mockData } from './mockApi';
